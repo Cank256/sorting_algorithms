2b227475acc4da85b597b52d0ab0dd88f9f7e109
Caleb Nkunze <nkunzecaleb@gmail.com>
2023-09-27 09:52:10 +0300

    main

---
+++ 0-bubble_sort.c
@@ -0,0 +1,52 @@
+#include "sort.h"
+
+/**
+ * swap - Swaps two integers.
+ * @a: Pointer to the first integer.
+ * @b: Pointer to the second integer.
+ */
+void swap(int *a, int *b)
+{
+int temp = *a;
+*a = *b;
+*b = temp;
+}
+
+/**
+ * bubble_sort - Sorts an array of integers in ascending order using Bubble sort.
+ * @array: Pointer to the array to be sorted.
+ * @size: Size of the array.
+ */
+void bubble_sort(int *array, size_t size)
+{
+int swapped;
+size_t i, j, k;
+
+for (i = 0; i < size - 1; i++)
+{
+swapped = 0;
+for (j = 0; j < size - i - 1; j++)
+{
+if (array[j] > array[j + 1])
+{
+swap(&array[j], &array[j + 1]);
+swapped = 1;
+
+for (k = 0; k < size; k++)
+{
+printf("%d", array[k]);
+if (k < size - 1)
+{
+printf(", ");
+}
+}
+printf("\n");
+}
+}
+
+if (swapped == 0)
+{
+break;
+}
+}
+}
---
+++ sort.h
@@ -0,0 +1,11 @@
+#ifndef SORT_H
+#define SORT_H
+
+#include <stdio.h>
+
+void swap(int *a, int *b);
+void print_array(int *a, int n);
+
+void bubble_sort(int *array, size_t size);
+
+#endif

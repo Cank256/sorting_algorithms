68b6cf76ac260c60823a497e352e10f3ae01952c
Caleb Nkunze <nkunzecaleb@gmail.com>
2023-09-27 11:58:54 +0300

    selection sort

--- sort.h
+++ sort.h
@@ -26,5 +26,6 @@ size_t print_list(const listint_t *h);
 
 void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
+void selection_sort(int *array, size_t size);
 
 #endif
